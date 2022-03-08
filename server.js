537f000de027421103fc7305aed8461ae75eac52
Amari Princivil <ajthacode88@gmail.com>
2022-03-07 23:40:04 -0500

    dayfour

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require(`express`);
+const routes = require(`./routes`);
+const db = require(`./db`);
+const logger = require(`morgan`);
+//console.log(routes);
+const PORT = process.env.PORT || 3400;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(logger(`dev`));
+app.use(`/api`, routes );
+db.on(`error`, console.error.bind(console, `MongoDB connection error`));
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
